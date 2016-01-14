93f221eda61aa922d8277fa9c36648f94c2f281c
tubwreck <tubwreck@gmail.com>
2016-01-13 20:34:05 -0500

    Updated from Brython Server: 1/13/2016 8:34:04 PM

--- tutorial.py
+++ tutorial.py
@@ -13,7 +13,7 @@ thinline = LineStyle(1, black)
 rectangle = RectangleAsset(50, 20, thinline, blue)
 
 # Now display a rectangle
-Sprite(rectangle)
-
+mySprite = Sprite(rectangle)
+mySprite.rotation(1)
 myapp = App()
 myapp.run()
